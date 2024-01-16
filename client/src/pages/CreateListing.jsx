5feb42e63d5e89b5f4408e486826466ec3e9f81e
Ani <Anish.kuber@gmail.com>
2024-01-16 15:52:37 +0530

    Added Image Slider to the Listing Page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -6,15 +6,15 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
-import {useSelector} from "react-redux";
-import {useNavigate} from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function CreateListing() {
   const [files, setFiles] = useState([]);
-  const {currentUser} = useSelector(state => state.user);
+  const { currentUser } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const [imageUploadError, setImageUploadError] = useState(false);
-  const [error,setError] = useState(false);
+  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const [uploading, setUploading] = useState(false);
   const [formData, setFormData] = useState({
@@ -32,7 +32,7 @@ export default function CreateListing() {
     furnished: false,
   });
 
-  console.log(formData);
+  
   const handleImageSubmit = (e) => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
       const promises = [];
@@ -59,6 +59,7 @@ export default function CreateListing() {
       setUploading(false);
     }
   };
+ 
   const storeImage = async (file) => {
     return new Promise((resolve, reject) => {
       const storage = getStorage(app);
@@ -84,65 +85,75 @@ export default function CreateListing() {
       );
     });
   };
+ 
   const handleRemoveImage = (index) => {
     setFormData({
       ...formData,
       imageUrls: formData.imageUrls.filter((_, i) => i !== index),
     });
   };
+ 
   const handleChange = (e) => {
-    if(e.target.id === "sale" || e.target.id ==="rent"){
+    if (e.target.id === "sale" || e.target.id === "rent") {
       setFormData({
         ...formData,
-        type: e.target.id
-      })
+        type: e.target.id,
+      });
     }
-    if(e.target.id === 'parking' || e.target.id ==='furnished' || e.target.id ==='offer'){
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
       setFormData({
         ...formData,
-        [e.target.id]: e.target.checked
-      })
+        [e.target.id]: e.target.checked,
+      });
     }
 
-    if(e.target.type === 'number' || e.target.type ==="text" || e.target.type ==="textarea"){
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
       setFormData({
-      ...formData,
-      [e.target.id]: e.target.value
-
-        
-      })
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
     }
   };
 
-  const handleSubmit = async(e)=>{
-      e.preventDefault();
-      try {
-        if(formData.imageUrls.length < 1) return setError('You must upload at least one image')
-        if(+formData.regularPrice < +formData.discountPrice) return setError('Discount price must be lower than regular price')
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError("You must upload at least one image");
+      if (+formData.regularPrice < +formData.discountPrice)
+        return setError("Discount price must be lower than regular price");
 
-        setLoading(true);
-        setError(false);
-        const res = await fetch('/api/listing/create',{
-          method:"POST",
-          headers:{
-            'Content-Type':'application/json',
-          },
-          body: JSON.stringify({
-            ...formData,
-            userRef: currentUser._id
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/listing/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
         }),
-        });
-        const data = await res.json();
-        setLoading(false);
-        if(data.success === false){
-          setError(data.message)
-        }
-        navigate(`/listing/${data._id}`);
-      } catch (error) {
-        setError(error.message)
-        setLoading(false);
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
       }
-  }
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -279,26 +290,24 @@ export default function CreateListing() {
                 <span className="text-sm">(₹ / Month)</span>
               </div>
             </div>
-            { formData.offer && (
+            {formData.offer && (
               <div className="flex items-center gap-2">
-              <input
-                type="number"
-                id="discountPrice"
-                min="0"
-                max="10000000"
-                required
-                className="p-3 border border-gray-300 rounded-lg"
-                onChange={handleChange}
-                value={formData.discountPrice}
-                
-              />
-              <div className="flex flex-col items-center">
-                <p>Discounted Price</p>
-                <span className="text-sm">(₹ / Month)</span>
+                <input
+                  type="number"
+                  id="discountPrice"
+                  min="0"
+                  max="10000000"
+                  required
+                  className="p-3 border border-gray-300 rounded-lg"
+                  onChange={handleChange}
+                  value={formData.discountPrice}
+                />
+                <div className="flex flex-col items-center">
+                  <p>Discounted Price</p>
+                  <span className="text-sm">(₹ / Month)</span>
+                </div>
               </div>
-            </div>
             )}
-            
           </div>
         </div>
         <div className="flex flex-col flex-1 gap-4">
@@ -332,10 +341,10 @@ export default function CreateListing() {
             className="p-3 bg-slate-700 text-white 
                 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
           >
-            {loading ? 'Creating...': "Create Listing"}
+            {loading ? "Creating..." : "Create Listing"}
           </button>
           {error && <p className="text-red-700 text-sm">{error}</p>}
-          
+
           <p className="text-red-700 text-sm">
             {imageUploadError && imageUploadError}
           </p>
